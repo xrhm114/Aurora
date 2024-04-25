59460cd84754e4529f71f584a285fb88219e76f0
blogforzjq <zjq1735252162@outlook.com>
2024-04-25 13:16:41 +0800

    This is a simple frame.

---
+++ src/theme.config.ts
@@ -0,0 +1,84 @@
+export const THEME_CONFIG: App.Locals['config'] = {
+  /** blog title */
+  title: "風を追うの開発ルーチン",
+  /** your name */
+  author: "Aurora",
+  /** website description */
+  desc: "Rediscory the beauty of typography",
+  /** your deployed domain */
+  website: "https://astro-theme-typography.vercel.app/",
+  /** your locale */
+  locale: "zh-cn",
+  /** theme style */
+  themeStyle: "auto",
+  /** your socials */
+  socials: [
+    {
+      name: "github",
+      href: "https://github.com/moeyua/astro-theme-typography",
+    },
+    {
+      name: "twitter",
+      href: "https://github.com/moeyua/astro-theme-typography",
+    },
+    {
+      name: "mastodon",
+      href: "https://github.com/moeyua/astro-theme-typography",
+    },
+    {
+      name: "rss",
+      href: "/atom.xml",
+    }
+  ],
+  /** your header info */
+  header: {
+    twitter: "@moeyua13",
+  },
+  /** your navigation links */
+  navs: [
+    {
+      name: "Posts",
+      href: "/posts/page/1",
+    },
+    {
+      name: "Archive",
+      href: "/archive",
+    },
+    {
+      name: "Categories",
+      href: "/categories"
+    },
+    {
+      name: "About",
+      href: "/about",
+    },
+  ],
+  /** your category name mapping, which the `path` will be shown in the url */
+  category_map: [
+    { name: "胡适", path: "hu-shi" },
+  ],
+  /** your comment provider */
+  comments: {
+    disqus: {
+      shortname: "typography-astro",
+    },
+    // giscus: {
+    //   repo: 'moeyua/astro-theme-typography',
+    //   repoId: 'R_kgDOKy9HOQ',
+    //   category: 'General',
+    //   categoryId: 'DIC_kwDOKy9HOc4CegmW',
+    //   mapping: 'title',
+    //   strict: '0',
+    //   reactionsEnabled: '1',
+    //   emitMetadata: '1',
+    //   inputPosition: 'top',
+    //   theme: 'light',
+    //   lang: 'zh-CN',
+    //   loading: 'lazy',
+    // },
+    // twikoo: {
+    //   envId: "https://twikoo-tau-flame.vercel.app",
+    // }
+  }
+}
+
